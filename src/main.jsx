da42e05449c252c3178b0264efffdd7d78d86ee5
salcrom <seya.28@gmail.com>
2024-07-29 12:29:16 +0200

    versión SPA sin protección de rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { HeroesApp } from "./HeroesApp.jsx";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <HeroesApp />
+        </BrowserRouter>
+    </React.StrictMode>
+);
